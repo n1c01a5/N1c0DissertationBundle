421f89f05485d2c5013ee8a1e4bfe13f957fc2d8
Wagner Nicolas <contact@wagner-nicolas.com>
2014-01-14 23:24:15 +0100

    Add file Argument.php

---
+++ Model/Argument.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace N1c0\DissertationBundle\Model;
+
+use DateTime;
+
+/**
+ * Storage agnostic argument dissertation object
+ */
+abstract class Argument implements ArgumentInterface
+{
+    /**
+     *Argument id 
+     *
+     * @var mixed
+     */
+    protected $id;
+
+    /**
+     * Title
+     *
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * Body
+     *
+     * @var string
+     */
+    protected $body;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param  string
+     * @return null
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param  string
+     * @return null
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function __toString()
+    {
+        return 'Argument #'.$this->getId();
+    }
+}
